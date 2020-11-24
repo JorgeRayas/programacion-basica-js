5dc3a549738be361f97687d686cb4f0ac8aad37e
Jorge Rayas <jorgerayasaleman@gmail.com>
2020-11-23 21:35:11 -0600

    Se inicializa el repositorio

---
+++ canvas/dibujo.js
@@ -0,0 +1,26 @@
+var d = document.getElementById("dibujito");
+var lienzo = d.getContext("2d");
+
+var lineas = 30;
+var l = 0;
+var color="#777";
+var distancia = 10;
+
+while (l < lineas) {
+  var c1 = l * distancia;
+  var c2 = l * distancia + distancia;
+  dibujarLinea("red", 0, c1, c2, 300); // esquina inf izq
+  dibujarLinea("green", 300, c1, 300 - c2, 300); // esquina inf der
+  dibujarLinea("blue", c1, 0, 300, c2); // esquina sup der
+  dibujarLinea("purple", 300-c1, 0, 0, c2); // esquina sup izq
+  l++;
+}
+
+function dibujarLinea(color, xInicial, yInicial, xFinal, yFinal) {
+  lienzo.beginPath();
+  lienzo.strokeStyle = color;
+  lienzo.moveTo(xInicial, yInicial);
+  lienzo.lineTo(xFinal, yFinal);
+  lienzo.stroke();
+  lienzo.closePath;
+}
---
+++ dom/codigo.js
@@ -0,0 +1,4 @@
+var ruta = window.location;
+console.log(ruta);
+document.write("Origen: " + ruta.origin);
+document.write("<br>Path: "+ruta.pathname);
