20a91b63c75a04c46ea9136b51b8c216fa698e4f
Jorge Rayas <jorgerayasaleman@gmail.com>
2020-11-28 19:11:54 -0600

    Se agregan inputs para obtener lineas en html Se agregan variables y duncion para obtener valores de inputs del html

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,9 +1,14 @@
+var texto = document.getElementById("texto_lineas");
+var boton = document.getElementById("botoncito");
+
+boton.addEventListener("click", dibujoPorClick);
+
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
 
 var lineas = 30;
 var l = 0;
-var color="#777";
+var color = "#777";
 var distancia = 10;
 
 while (l < lineas) {
@@ -12,7 +17,7 @@ while (l < lineas) {
   dibujarLinea("red", 0, c1, c2, 300); // esquina inf izq
   dibujarLinea("green", 300, c1, 300 - c2, 300); // esquina inf der
   dibujarLinea("blue", c1, 0, 300, c2); // esquina sup der
-  dibujarLinea("purple", 300-c1, 0, 0, c2); // esquina sup izq
+  dibujarLinea("purple", 300 - c1, 0, 0, c2); // esquina sup izq
   l++;
 }
 
@@ -23,4 +28,11 @@ function dibujarLinea(color, xInicial, yInicial, xFinal, yFinal) {
   lienzo.lineTo(xFinal, yFinal);
   lienzo.stroke();
   lienzo.closePath;
+}
+
+function dibujoPorClick() {
+  var x = texto.value;
+  console.log(texto.value);
+  alert("No me toques ahi " + x);
+
 }
