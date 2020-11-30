53581e886f143e72a6851237cc6d82fd069dfb9a
Jorge Rayas <jorgerayasaleman@gmail.com>
2020-11-29 22:36:32 -0600

    Se cambia codigo para omitir lo numeros que son multiplos y solo dejar las frases fizz y buzz

--- fizzbuzz/fizz.js
+++ fizzbuzz/fizz.js
@@ -1,12 +1,12 @@
 var agregado = "";
 for (let index = 1; index <= 100; index++) {
+  agregado = index;
   if (index % 3 == 0) {
     agregado = "fizz";
   }
   if (index % 5 == 0) {
-    agregado += "buzz";
+    agregado = agregado == index ? "buzz" : agregado + "buzz";
   }
 
-  document.write(index + ".- " + agregado + "<br>");
-  agregado = "";
+  document.write(agregado + "<br>");
 }
