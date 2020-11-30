fede587ed149093c6ad6cf231681971b399b3302
Jorge Rayas <jorgerayasaleman@gmail.com>
2020-11-29 22:00:26 -0600

    Se agrega codigo para mover a un cerdo

--- villaPlatzi/platzi.js
+++ villaPlatzi/platzi.js
@@ -1,11 +1,12 @@
 var vp = document.getElementById("villaplatzi");
 var papel = vp.getContext("2d");
 
+var teclas = { LEFT: 37, UP: 38, RIGHT: 39, DOWN: 40 };
 var fondo = { url: "tile.png", cargaOK: false };
 var vaca = { url: "vaca.png", cargaOK: false };
 var pollo = { url: "pollo.png", cargaOK: false };
 var cerdo = { url: "cerdo.png", cargaOK: false };
-var cerdoCorredor = { url: "cerdo.png", cargaOK: false, cerdoX: 0, cerdoY: 0 };
+var cerdoCorredor = { url: "cerdo.png", cargaOK: false, coordX: 0, coordY: 0 };
 
 fondo.imagen = new Image();
 fondo.imagen.src = fondo.url;
@@ -27,13 +28,19 @@ cerdoCorredor.imagen = new Image();
 cerdoCorredor.imagen.src = cerdoCorredor.url;
 cerdoCorredor.imagen.addEventListener("load", cargarCerdoCorredor);
 
+document.addEventListener("keydown", moverCerdoCorredor);
+
 function dibujar() {
   if (fondo.cargaOK) {
     papel.drawImage(fondo.imagen, 0, 0);
   }
 
   if (cerdoCorredor.cargaOK) {
-    papel.drawImage(cerdoCorredor.imagen, cerdoCorredor.cerdoX, cerdoCorredor.cerdoY);
+    papel.drawImage(
+      cerdoCorredor.imagen,
+      cerdoCorredor.coordX,
+      cerdoCorredor.coordY
+    );
   }
 
   dibujoAleatorio(vaca, aleatorio(0, 6));
@@ -74,10 +81,44 @@ function cargarCerdos() {
 }
 
 function cargarCerdoCorredor() {
-    cerdoCorredor.cargaOK = true;
-    dibujar();
-  }
+  cerdoCorredor.cargaOK = true;
+  dibujar();
+}
 
 function aleatorio(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function moverCerdoCorredor(evento) {
+  var movimiento = 10;
+  switch (evento.keyCode) {
+    case teclas.UP:
+      if (cerdoCorredor.coordY - movimiento >= 0) {
+        cerdoCorredor.coordY -= movimiento;
+        dibujar();
+      }
+      break;
+    case teclas.DOWN:
+      if (cerdoCorredor.coordY + movimiento <= 420) {
+        cerdoCorredor.coordY += movimiento;
+        dibujar();
+      }
+      break;
+    case teclas.LEFT:
+      if (cerdoCorredor.coordX - movimiento >= 0) {
+        cerdoCorredor.coordX -= movimiento;
+        dibujar();
+      }
+      break;
+    case teclas.RIGHT:
+      if (cerdoCorredor.coordX + movimiento <= 420) {
+        cerdoCorredor.coordX += movimiento;
+        dibujar();
+      }
+      break;
+    default:
+      console.log("Otra tecla");
+  }
+
+  //console.log("x=" + x + ", y=" + y);
 }
